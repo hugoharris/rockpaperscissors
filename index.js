40ab291668f452dde507d8cac917e1053dba3f57
Kevin Harris <hugokharris@gmail.com>
2023-01-16 13:55:51 -0700

    finish first js functions for game outcome and computer choice

---
+++ index.js
@@ -0,0 +1,21 @@
+// determine outcome of a single game
+function determineOutcome(player, computer) {
+    if (player === computer) {
+        return 'tie';
+    } else if ((player === 'rock' && computer === 'scissors') ||
+                (player === 'scissors' && computer === 'paper') ||
+                (player === 'paper' && computer === 'rock')) {
+        return 'player';
+    } else {
+        return 'computer';
+    }
+}
+
+// randomly determine computer choice
+function determineComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const x = Math.floor(Math.random() * 3);
+    return choices[x];
+}
+
+console.log(determineComputerChoice());

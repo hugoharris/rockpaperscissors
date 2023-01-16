45709fa0c20414ab4dc18bba56a354488d7c323b
Kevin Harris <hugokharris@gmail.com>
2023-01-16 14:16:01 -0700

    add DOM elements and event listeners to js

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+// add DOM display elements
+const playerScore = document.querySelector('.player-score');
+const computerScore = document.querySelector('.computer-score');
+const computerChoice = document.querySelector('.computer-choice');
+const gameResult = document.querySelector('.game-result');
+
+// add DOM button elements
+const rockBtn = document.querySelector('.rock-btn');
+const paperBtn = document.querySelector('.paper-btn');
+const scissorsBtn = document.querySelector('.scissors-btn');
+const newGameBtn = document.querySelector('.new-game-btn');
+
+// add event listeners
+
+rockBtn.addEventListener('click', () => handleClick('rock'));
+paperBtn.addEventListener('click', () => handleClick('paper'));
+scissorsBtn.addEventListener('click', () => handleClick('scissors'));
+newGameBtn.addEventListener('click', () => handleClick('newgame'));
+
 // determine outcome of a single game
 function determineOutcome(player, computer) {
     if (player === computer) {
